0485ed45768ade92aea37904df46bef07fa406c0
Jacob Ledbetter <ledbetterj1@wit.edu>
2020-04-07 10:37:41 -0400

    Added a start menu of sorts.

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,16 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.geometry.Pos;
 
 /**
  * Home of main, most GUI logic, and all the game logic.
@@ -21,6 +24,37 @@ public class Game extends Application {
 	// TODO: GUI and game logic (oof).
 	static long frameCount = 0; // How many frames have passed(and what frame your on)(level independent)
 	static long currentTime = 1;
+	
+	
+	public void setupStartScene(Scene s, EventHandler<ActionEvent> e) {
+		Pane basePane = new Pane();
+		VBox centerPane = new VBox();
+		Text title = new Text("Sheild Heero");
+		Button start = new Button("Start");
+		Button exit = new Button("Quit");
+		
+		basePane.getChildren().add(exit);
+		basePane.getChildren().add(centerPane);
+		
+		centerPane.getChildren().add(title);
+		centerPane.getChildren().add(start);
+		centerPane.setLayoutX((s.getWidth()/2)-45);
+		centerPane.setLayoutY((s.getHeight()/2)-30);
+		centerPane.setAlignment(Pos.CENTER);
+		
+		EventHandler<ActionEvent> exitAction = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+		};
+		
+		exit.setOnAction(exitAction);
+		start.setOnAction(e);
+		
+		s.setRoot(basePane);
+		
+	}
 	
 	public static void main(String[] args) {
 		System.out.printf("Hello, world!");
@@ -45,8 +79,17 @@ public class Game extends Application {
 		pane.getChildren().add(arrow);
 		
 		Scene scene = new Scene(pane, 200, 200);
+		Scene startScreen = new Scene(new Pane(), 500, 500);
+		EventHandler<ActionEvent> startAction = new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				mainStage.setScene(scene);
+			}
+		};
+		setupStartScene(startScreen, startAction);
+		
 		mainStage.setTitle("ugh");
-		mainStage.setScene(scene);
+		mainStage.setScene(startScreen);
 		mainStage.show();
 		
 		long initTime = System.currentTimeMillis();
@@ -69,6 +112,8 @@ public class Game extends Application {
 		Timeline a = new Timeline(new KeyFrame(Duration.millis(FPS60), frameAction)); // Thing the make frames go brrr.
 		a.setCycleCount(Timeline.INDEFINITE);
 		a.play();
+		
+		
 		
 		/*
 		scene.requestFocus();
